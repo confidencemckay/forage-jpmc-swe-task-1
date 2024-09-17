c416474d64ffd3a0015d75a1873626c2a6edda7a
Confidence Makofane <confidencemckay@gmail.com>
2024-09-17 21:15:26 +0200

    Updated code with Unit Tests

--- client3.py
+++ client3.py
@@ -61,3 +61,38 @@ if __name__ == "__main__":
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
         print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
+
+#Unit Tests
+def test_getDataPoint_calculatePrice(self):
+    quotes = [
+        {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453',
+         'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+        {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453',
+         'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+
+    #Loop through quotes and make assertions for each
+    for quote in quotes:
+        stock = quote['stock']
+        top_bid_price = quote['top_bid']['price']
+        top_ask_price = quote['top_ask']['price']
+        expected_price = (top_bid_price + top_ask_price) / 2
+
+        #Assert the output of getDataPoint() is correct
+        self.assertEqual(getDataPoint(quote), (stock, top_bid_price, top_ask_price, expected_price))
+
+def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+    quotes = [
+        {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+        {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+
+    #Loop through quotes and make assertions for each
+    for quote in quotes:
+        stock = quote['stock']
+        top_bid_price = quote['top_bid']['price']
+        top_ask_price = quote['top_ask']['price']
+        expected_price = (top_bid_price + top_ask_price) / 2
+
+        #Assert the output of getDataPoint() is correct
+        self.assertEqual(getDataPoint(quote), (stock, top_bid_price, top_ask_price, expected_price))
